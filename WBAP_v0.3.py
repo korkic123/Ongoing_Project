a2ef621b8c2b6d2fcf4dfd7e3132d99059aed355
korkic123 <40509734+korkic123@users.noreply.github.com>
2020-04-14 09:16:25 +0900

    Update WBAP_v0.3.py
    # history_db()
    # cookies_db()
    # favicons_db()
    # logindata_db()
    # 분석 DB 추가예정

--- WBAP_v0.3.py
+++ WBAP_v0.3.py
@@ -57,43 +57,37 @@ def analyze_db(lt) :
         if os.path.exists(lt + "\\AppData\\Local\\Naver\\Naver Whale\\User Data\\Default\\History") :
             db = lt + "\\AppData\\Local\\Naver\\Naver Whale\\User Data\\Default\\History"
 
-    conn = sqlite3.connect(db)
-    cur = conn.cursor()
-    cur2 = conn.cursor()
+            conn = sqlite3.connect(db)
+            cur = conn.cursor()
+            cur2 = conn.cursor()
 
-    for row in cur.execute("select id, last_visit_time, url, title, visit_count from urls order by last_visit_time desc") :
-    # count = 연번
+            for row in cur.execute("select id, last_visit_time, url, title, visit_count from urls order by last_visit_time desc") :
+            # count = 연번
 
-        count += 1
-        temp += str(count) + ","
-        data_flag = 0
+                count += 1
+                temp += str(count) + ","
+                data_flag = 0
 
-        for m in range(1, 5) :
-            if m == 1 :
-                temp += str(convert_ct_2_kst(row[m])) + ","
-                temp += "Naver Whale Browser,"
-                for row2 in cur2.execute("select url_id, term from keyword_search_terms") :
-                    if row[0] == row2[0] :
-                        temp += "검색," + row2[1] + ","
-                        data_flag = 1
-                if data_flag == 0 :
-                    temp += ",,"
-            else :
-                temp += str(row[m]) + ","
-        temp += "\n"
+                for m in range(1, 5) :
+                    if m == 1 :
+                        temp += str(convert_ct_2_kst(row[m])) + ","
+                        temp += "Naver Whale Browser,"
+                        for row2 in cur2.execute("select url_id, term from keyword_search_terms") :
+                            if row[0] == row2[0] :
+                                temp += "검색," + row2[1] + ","
+                                data_flag = 1
+                        if data_flag == 0 :
+                            temp += ",,"
+                    else :
+                        temp += str(row[m]) + ","
+                temp += "\n"
 
-    cur.close()
-    cur2.close()
+            cur.close()
+            cur2.close()
 
     f = open(os.environ["USERPROFILE"] + "\\Desktop\\" + user + "_WBAP_Result.csv", "w")
     f.write(temp)
     f.close
-
-# history_db()
-# cookies_db()
-# favicons_db()
-# logindata_db()
-# 분석 DB 추가예정
 
 os.system("cls")
 
